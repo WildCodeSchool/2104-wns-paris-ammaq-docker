b977875613c19ee0e85f96528af27214cc4c7a28
Quentin Liger <qliger.dev@gmail.com>
2021-10-06 11:44:00 +0200

    add compose_project_name to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 echo "PORT=$PORT"
 git fetch origin && git reset --hard origin/main && git clean -f -d
 git submodule update --remote
-GATEWAY_PORT=$PORT docker-compose -f docker-compose.prod.yml up --build -d
+GATEWAY_PORT=$PORT COMPOSE_PROJECT_NAME=staging docker-compose -f docker-compose.prod.yml up --build -d
